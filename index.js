5119f09816b9a4f2e1b80fa818ddbbcf7b9f4cbb
gabrielmontrone <montrone283@gmail.com>
2024-09-10 10:12:44 -0300

    adicionando dependencia do google gemini

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors()); 
-// app.use(express.json());
+app.use(express.json());
 
-// const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY); 
-// const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY); 
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
