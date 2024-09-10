b01ffe86763d70ea9549c733acdcb3f15b89fbda
gabrielmontrone <montrone283@gmail.com>
2024-09-10 10:10:18 -0300

    uptade index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors'); 
+
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors()); 
-app.use(express.json());
+// app.use(express.json());
 
-
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY); 
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+// const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY); 
+// const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
