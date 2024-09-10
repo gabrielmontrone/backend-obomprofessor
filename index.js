7a4dc84ff3adbae9119b3b2cd57c3ca130ce4323
gabrielmontrone <montrone283@gmail.com>
2024-09-09 21:44:47 -0300

    resolvendo conflitos na api relacionado ao cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const cors = require('cors'); // Importando o middleware CORS
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors()); // Adiciona o middleware CORS para permitir requisições de diferentes origens
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port ${port}');
+    console.log(`Server is running on port ${port}`);
 });
-
-
